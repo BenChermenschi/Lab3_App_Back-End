eeb9d2ceeda38e1745f460bef92fff903d6cc01b
Lien_git <chermenschi.ben@gmail.com>
2021-06-16 19:50:11 +0200

    Update vragenlijstController.js

--- controllers/vragenlijstController.js
+++ controllers/vragenlijstController.js
@@ -185,7 +185,7 @@ exports.getRecentVragenlijst=function(req,res,next){
         console.log('searching');
 
         Vragenlijst.find()
-        .sort({datum:-1}).limit(1)
+        .sort({ field: -_id }).limit(1)
     .populate({
         path:'gebruiker',
         options: { retainNullValues: true },
@@ -262,7 +262,7 @@ exports.getRecentVragenlijstByUser=function(req,res,next){
                 console.log('searching');
 
                 Vragenlijst.find({gebruiker:req.body._id})
-                .sort({datum:-1}).limit(1)
+                .sort({ field: -_id }).limit(1)
             .populate({
                 path:'gebruiker',
                 options: { retainNullValues: true },
