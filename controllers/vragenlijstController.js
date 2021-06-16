4a746dea9f935fb7d1f43e9d8e713aabf8c96348
Lien_git <chermenschi.ben@gmail.com>
2021-06-16 23:11:02 +0200

    get recent dates?

--- controllers/vragenlijstController.js
+++ controllers/vragenlijstController.js
@@ -185,7 +185,7 @@ exports.getRecentVragenlijst=function(req,res,next){
         console.log('searching');
 
         Vragenlijst.find()
-        .sort({ createdAt: -1 }).limit(1)
+        .sort('-date').limit(1)
     .populate({
         path:'gebruiker',
         options: { retainNullValues: true },
@@ -262,7 +262,7 @@ exports.getRecentVragenlijstByUser=function(req,res,next){
                 console.log('searching');
 
                 Vragenlijst.find({gebruiker:req.body._id})
-                .sort({ createdAt: -1 }).limit(1)
+                .sort('-date').limit(1)
             .populate({
                 path:'gebruiker',
                 options: { retainNullValues: true },
